155708cd6051c4970cd9261d326cbb8465297adc
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-09 01:55:55 +0000

    Done.

--- spec/instance_methods_spec.rb
+++ spec/instance_methods_spec.rb
@@ -32,3 +32,37 @@ describe 'Dog' do
     end
   end
 end
+describe 'Person' do
+  it 'is defined' do
+    expect(defined?(Person)).to be_truthy
+    expect(Person).to be_a(Class)
+  end
+
+  describe '#talk' do
+    it 'responds to the #talk instance method' do
+      ada = Person.new
+      expect(ada).to respond_to(:talk)
+    end
+
+    it 'puts "Hello World!"' do
+      expect($stdout).to receive(:puts).with("Hello World!")
+
+      ada = Person.new
+      ada.talk
+    end
+  end
+
+  describe '#walk' do
+    it 'responds to the #walk instance method' do
+      ada = Person.new
+      expect(ada).to respond_to(:walk)
+    end
+
+    it 'puts "The Person is walking"' do
+      expect($stdout).to receive(:puts).with("The Person is walking")
+
+      ada = Person.new
+      ada.walk
+    end
+  end
+end
